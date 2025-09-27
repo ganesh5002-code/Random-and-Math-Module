077faf110ddc6e55a951f05b67bbe2f8d2759870
ganesh5002-code <hethaksh.y@gmail.com>
2025-09-27 12:51:18 +0800

    Initial commit

---
+++ sin,cos,tan.py
@@ -0,0 +1,5 @@
+import math
+num = float(input("Enter a number"))
+print("The cosine of", num, "is equal to", math.cos(num))
+print("The tan of", num, "is equal to", math.tan(num))
+print("The sine of", num, "is equal to", math.sin(num))
